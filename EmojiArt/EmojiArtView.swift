0fd3b38116f7797131353e3da732b2482b6b6e96
kryp17 <krupakhar978@gmail.com>
2020-04-06 15:20:24 +0530

    Added utilities and basic drop functionality

---
+++ EmojiArt/EmojiArtView.swift
@@ -0,0 +1,20 @@
+//
+//  EmojiArtView.swift
+//  EmojiArt
+//
+//  Created by krupakhar gandeepan on 06/04/20.
+//  Copyright © 2020 krupakhar. All rights reserved.
+//
+
+import UIKit
+
+class EmojiArtView: UIView {
+
+    var backGroundImage: UIImage? { didSet{setNeedsDisplay(); setNeedsLayout()} }
+    
+    override func draw(_ rect: CGRect) {
+        backGroundImage?.draw(in: bounds)
+    }
+    
+
+}
--- EmojiArt/EmojiArtViewController.swift
+++ EmojiArt/EmojiArtViewController.swift
@@ -8,23 +8,40 @@
 
 import UIKit
 
-class EmojiArtViewController: UIViewController {
+class EmojiArtViewController: UIViewController,UIDropInteractionDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var dropZone: UIView!{
+        didSet{
+            dropZone.addInteraction(UIDropInteraction(delegate: self))
+        }
+    }
+
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: NSURL.self) && session.canLoadObjects(ofClass: UIImage.self)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        return UIDropProposal(operation: .copy)
     }
-    */
+    
+    var imageFetcher = imageFetcher!
+    
+    
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        imageFetcher = imageFetcher(){ (url, image) in
+            
+            
+        }
+        session.loadObjects(ofClass: NSURL.self){ nsurls in
+            
+        }
+        session.loadObjects(ofClass: UIImage.self){ images in
+            
+        }
+    }
+    
+    @IBOutlet weak var emojiArtView: EmojiArtView!
 
+    
 }
