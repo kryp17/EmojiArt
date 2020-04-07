68dab481ec29262ff7ff8f2c9613a86c29f36604
kryp17 <krupakhar978@gmail.com>
2020-04-07 09:55:43 +0530

    Added preferred view mode

--- EmojiArt/EmojiArtDocumentTableViewController.swift
+++ EmojiArt/EmojiArtDocumentTableViewController.swift
@@ -38,6 +38,14 @@ class EmojiArtDocumentTableViewController: UITableViewController {
         tableView.reloadData()
     }
     
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        if splitViewController?.preferredDisplayMode != .primaryOverlay{
+            splitViewController?.preferredDisplayMode = .primaryOverlay
+        }
+    }
+    
+    
 
     /*
     // Override to support conditional editing of the table view.
